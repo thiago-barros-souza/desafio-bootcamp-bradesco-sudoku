17502d6d6c8be40f619009dd0706847c89f6cf6f
Thiago Barros <thiago.barros@perinity.com>
2025-03-18 23:31:06 -0300

    Estudando o código e aplicando melhorias

--- src/br/com/dio/model/GameStatusEnum.java
+++ src/br/com/dio/model/GameStatusEnum.java
@@ -2,18 +2,16 @@ package br.com.dio.model;
 
 public enum GameStatusEnum {
 
-    NON_STARTED("não iniciado"),
-    INCOMPLETE("incompleto"),
-    COMPLETE("completo");
+	NON_STARTED("não iniciado"), INCOMPLETE("incompleto"), COMPLETE("completo");
 
-    private String label;
+	private String label;
 
-    GameStatusEnum(final String label){
-        this.label = label;
-    }
+	GameStatusEnum(final String label) {
+		this.label = label;
+	}
 
-    public String getLabel() {
-        return label;
-    }
+	public String getLabel() {
+		return label;
+	}
 
 }
